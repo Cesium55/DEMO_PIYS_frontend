042b9c5bb47bf2be35f455762d78c302ecea654b
makenti <mksmtshkn@gmail.com>
2025-05-12 23:38:02 +0300

    plyr player

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,8 +1,8 @@
 <template>
 
     <div>
-        <video id="movie_player" controls ref="player" :key="$props.video_src">
-            <source :src="$props.video_src" type="video/mp4">
+        <video ref="videoRef" class="plyr__video-embed" playsinline controls v-if="video_src">
+            <source :src="video_src" type="video/mp4" />
         </video>
     </div>
 
@@ -12,17 +12,32 @@
 
 <script setup>
 
-import { ref, watch } from 'vue';
+import { ref, watch, onMounted } from 'vue';
+
+import Plyr from 'plyr'
+import 'plyr/dist/plyr.css'
 
 const props = defineProps({
     video_src: String
 })
 
+const videoRef = ref(null)
+let player = null
 
-const player = ref(null)
+onMounted(() => {
+    if (videoRef.value) {
+        player = new Plyr(videoRef.value)
+    }
+})
 
 
-
+watch(() => props.video_src, (newSrc) => {
+    if (videoRef.value && newSrc) {
+        // Переустанавливаем источник и перезагружаем
+        videoRef.value.src = newSrc
+        videoRef.value.load()
+    }
+})
 </script>
 
 <style scoped>
